588ad532ec1fec94b0a9adfcd79f70fb8c164410
TheBrain0110 <tonofpudding@gmail.com>
2018-06-13 02:28:28 -0400

    Nerf output rate

--- prototypes/recipe/fluid-recipe.lua
+++ prototypes/recipe/fluid-recipe.lua
@@ -1,11 +1,13 @@
 data.raw["recipe"]["coal-liquefaction"].ingredients =
 {
-  {type="item", name="coal", amount=10},
+  {type="item", name="coal", amount=15},
   {type="fluid", name="heavy-oil", amount=20},
   {type="fluid", name="steam", amount=100}
 }
 
 data.raw["recipe"]["coal-liquefaction"].results =
 {
-  {type="fluid", name="heavy-oil", amount=60}
+  {type="fluid", name="heavy-oil", amount=40}
 }
+
+data.raw["recipe"]["coal-liquefaction"].energy_required = 20
