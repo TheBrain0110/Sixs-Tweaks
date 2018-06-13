4140c4d04021e5840245f289edb8b54eb0187a16
TheBrain0110 <tonofpudding@gmail.com>
2018-06-13 02:33:25 -0400

    Try 1 oil/s, 1 coal/oil?

--- prototypes/recipe/fluid-recipe.lua
+++ prototypes/recipe/fluid-recipe.lua
@@ -1,13 +1,13 @@
 data.raw["recipe"]["coal-liquefaction"].ingredients =
 {
-  {type="item", name="coal", amount=15},
-  {type="fluid", name="heavy-oil", amount=20},
+  {type="item", name="coal", amount=10},
+  {type="fluid", name="heavy-oil", amount=10},
   {type="fluid", name="steam", amount=100}
 }
 
 data.raw["recipe"]["coal-liquefaction"].results =
 {
-  {type="fluid", name="heavy-oil", amount=40}
+  {type="fluid", name="heavy-oil", amount=20}
 }
 
-data.raw["recipe"]["coal-liquefaction"].energy_required = 20
+data.raw["recipe"]["coal-liquefaction"].energy_required = 10
