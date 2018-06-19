43f8449bc867059b0587a1e1d329527fb7bfadb7
TheBrain0110 <tonofpudding@gmail.com>
2018-06-19 00:38:06 -0400

    Increase Heavy Oil rate

--- prototypes/recipe/fluid-recipe.lua
+++ prototypes/recipe/fluid-recipe.lua
@@ -1,13 +1,13 @@
 data.raw["recipe"]["coal-liquefaction"].ingredients =
 {
-  {type="item", name="coal", amount=15},
+  {type="item", name="coal", amount=10},
   {type="fluid", name="crude-oil", amount=5},
-  {type="fluid", name="steam", amount=100}
+  {type="fluid", name="steam", amount=60}
 }
 
 data.raw["recipe"]["coal-liquefaction"].results =
 {
-  {type="fluid", name="heavy-oil", amount=20}
+  {type="fluid", name="heavy-oil", amount=25}
 }
 
-data.raw["recipe"]["coal-liquefaction"].energy_required = 10
+data.raw["recipe"]["coal-liquefaction"].energy_required = 5
