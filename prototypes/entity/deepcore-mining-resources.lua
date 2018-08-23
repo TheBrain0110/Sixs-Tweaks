91b5401279d0a11fc1eca247d7bfd1ea21c8c874
TheBrain0110 <tonofpudding@gmail.com>
2018-08-22 23:02:54 -0400

    Fix Startup errors

--- prototypes/entity/deepcore-mining-resources.lua
+++ prototypes/entity/deepcore-mining-resources.lua
@@ -1,12 +1,21 @@
-data.raw["resource"]["vtk-deepcore-mining-crack"].fluid_amount = 50,
+data.raw["resource"]["vtk-deepcore-mining-crack"].fluid_amount = 50
 data.raw["resource"]["vtk-deepcore-mining-crack"].required_fluid = "heavy-oil"
 
 local ores = {"iron-ore", "copper-ore", "coal", "stone", "uranium-ore"}
 
 for _, ore in pairs(ores) do
-  data.raw["resource"][ore.."-patch"].results.name = "vtk-deepcore-mining-"..ore.."-chunk"
+  data.raw["resource"][ore.."-patch"].minable.results =
+  {
+    {
+      type = "item",
+      name = "vtk-deepcore-mining-"..ore.."-chunk",
+      amount_min = 1,
+      amount_max = 1,
+      probability = 1
+    }
+  }
   data.raw["resource"][ore.."-patch"].minable.mining_time = data.raw["resource"][ore.."-patch"].minable.mining_time * 60
-  if data.raw["resource"][ore.."-patch"].minable.fluid_amount > 0
+  if data.raw["resource"][ore.."-patch"].minable.fluid_amount
     then data.raw["resource"][ore.."-patch"].minable.fluid_amount = data.raw["resource"][ore.."-patch"].minable.fluid_amount * 10
   end
   data.raw["resource"][ore.."-patch"].infinite_depletion_amount = 1
