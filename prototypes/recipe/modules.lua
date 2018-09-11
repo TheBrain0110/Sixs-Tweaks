b9b399160f7857c82807338459a187ac4810bcc5
TheBrain0110 <tonofpudding@gmail.com>
2018-09-10 22:18:07 -0400

    Add lvl-1 modules to merged module ingredients

--- prototypes/recipe/modules.lua
+++ prototypes/recipe/modules.lua
@@ -1,13 +1,8 @@
 local moduletypes = {"raw-speed-module-", "green-module-", "raw-productivity-module-"}
 
 for _, moduletype in pairs(moduletypes) do
-    for i = 1,8 do
-        -- There's 2 recipes for each merged module by default
-        -- One made with raw components, and one that just takes completed modules
-        -- I want to remove the raw components recipe, and rework the combining one.
-        bobmods.lib.item.remove(data.raw.recipe, moduletype..i) -- May need to just disable, tech unlock might complain if recipe doesn't exist
-        
-        local recipe = moduletype..i.."-combine"
-        table.insert(data.raw.recipe[recipe].ingredients, {"alien-artifact", 1})
+    for i = 2,8 do
+        table.insert(data.raw.recipe[moduletype..i].ingredients, {moduletype..i-1, 1})
+        table.insert(data.raw.recipe[moduletype..i.."-combine"].ingredients, {moduletype..i-1, 1})
     end
 end
