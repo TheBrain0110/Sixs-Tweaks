2d1b715e8312e4bff545db22b41fbb9285f59a7f
TheBrain0110 <tonofpudding@gmail.com>
2018-09-11 12:14:07 -0400

    Require 2x modules for each level of combined module
    Also switch to stdlib table.insert, remove bobslib dependency

--- prototypes/recipe/modules.lua
+++ prototypes/recipe/modules.lua
@@ -1,14 +1,22 @@
 local moduletypes = {"raw-speed-module-", "green-module-", "raw-productivity-module-"}
 
 for _, moduletype in pairs(moduletypes) do
-    -- data.raw.recipe[moduletype..1] = nil
-    data.raw.recipe[moduletype..1].hidden = true
+    for i = 1,8 do
+        -- Remove raw ingredients recipe variants, leave only module-combining recipes
+        -- Hidden is almost as good as removed for my purposes...
+        data.raw.recipe[moduletype..i].hidden = true
+    end
+    for i = 1,8 do
+        -- Require 2x modules for each level of combined module
+        for _, ingredient in pairs(data.raw.recipe[moduletype..i.."-combine"].ingredients) do
+            -- Sometimes recipes use named fields, sometimes they're just lists. See bobmods.lib.item.basic_item for reference.
+            if ingredient.amount then ingredient.amount = ingredient.amount * 2
+            else ingredient[2] = ingredient[2] * 2
+            end
+        end
+    end
     for i = 2,8 do
-        -- remove raw-components recipe
-        -- data.raw.recipe[moduletype..i] = nil -- Successfully removes recipe, but bob's complains about it missing. Maybe if this was moved to data-final-fixes...
-        data.raw.recipe[moduletype..i].hidden = true -- hidden is almost as good as disabled for my purposes...
-
-        -- Add module level-1 to ingredients for Combined recipes
-        bobmods.lib.recipe.add_ingredient(moduletype..i.."-combine", {moduletype..i-1, 1})
+       -- Add module level-1 to ingredients for Combined recipes
+        table.insert(data.raw.recipe[moduletype..i.."-combine"].ingredients, {moduletype..i-1, 1})
     end
 end
