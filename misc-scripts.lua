62c0f741e52627ff72ebcf64134d29b390b87847
TheBrain0110 <tonofpudding@gmail.com>
2018-09-11 00:59:03 -0400

    Hide raw-components recipes
    Works just as well as deleting or disabling them, with less breaking things.

--- misc-scripts.lua
+++ misc-scripts.lua
@@ -1,7 +1,17 @@
+-- Double Ore patch amounts
 /c surface = game.player.surface
 local patches = {"iron-ore-patch", "copper-ore-patch", "coal-patch", "stone-patch", "uranium-ore-patch"}
 for _, ore in pairs(surface.find_entities_filtered({type="resource"})) do
     for _,patchname in pairs(patches) do
         if ore.name == patchname then ore.amount = ore.amount * 2 end
+    end
+end
+
+-- Disable secondary set of combined module recipes
+/c local moduletypes = {"raw-speed-module-", "green-module-", "raw-productivity-module-"}
+
+for _, moduletype in pairs(moduletypes) do
+    for i = 2,8 do
+        game.forces.player.recipes[moduletype..i].enabled = false
     end
 end
--- prototypes/recipe/modules.lua
+++ prototypes/recipe/modules.lua
@@ -1,10 +1,12 @@
 local moduletypes = {"raw-speed-module-", "green-module-", "raw-productivity-module-"}
 
 for _, moduletype in pairs(moduletypes) do
+    -- data.raw.recipe[moduletype..1] = nil
+    data.raw.recipe[moduletype..1].hidden = true
     for i = 2,8 do
-        -- Change Raw Ingredients recipe to require 2x level-1 combined modules, from 1x
-        bobmods.lib.recipe.remove_ingredient(moduletype..i, moduletype..i-1)
-        bobmods.lib.recipe.add_ingredient(moduletype..i, {moduletype..i-1, 2})
+        -- remove raw-components recipe
+        -- data.raw.recipe[moduletype..i] = nil -- Successfully removes recipe, but bob's complains about it missing. Maybe if this was moved to data-final-fixes...
+        data.raw.recipe[moduletype..i].hidden = true -- hidden is almost as good as disabled for my purposes...
 
         -- Add module level-1 to ingredients for Combined recipes
         bobmods.lib.recipe.add_ingredient(moduletype..i.."-combine", {moduletype..i-1, 1})
