5d7af2088083521acb4e2b65a5a5d8671ab78404
TheBrain0110 <tonofpudding@gmail.com>
2020-10-01 16:19:29 -0400

    Commit final unsaved old script

--- misc-scripts.lua
+++ misc-scripts.lua
@@ -1,9 +1,18 @@
 -- Double Ore patch amounts
-/c surface = game.player.surface
+/c local surface = game.player.surface
 local patches = {"iron-ore-patch", "copper-ore-patch", "coal-patch", "stone-patch", "uranium-ore-patch"}
 for _, ore in pairs(surface.find_entities_filtered({type="resource"})) do
-    for _,patchname in pairs(patches) do
+    for _, patchname in pairs(patches) do
         if ore.name == patchname then ore.amount = ore.amount * 2 end
+    end
+end
+
+-- Increase Vanilla Ore field amounts
+/c local surface = game.player.surface
+local patches = {"iron-ore", "copper-ore", "coal", "stone", "uranium-ore", "crude-oil"}
+for _, ore in pairs(surface.find_entities_filtered({type="resource"})) do
+    for _, patchname in pairs(patches) do
+        if ore.name == patchname then ore.amount = ore.amount * 3 end
     end
 end
 
@@ -15,3 +24,19 @@ for _, moduletype in pairs(moduletypes) do
         game.forces.player.recipes[moduletype..i].enabled = false
     end
 end
+
+-- Get Mapgen autoplace (resource) settings
+/c game.player.print(serpent.block(surface.map_gen_settings.autoplace_controls))
+-- or
+/c game.write_file("mapgen_settings.txt", serpent.block(surface.map_gen_settings.autoplace_controls))
+
+-- Change mapgen autoplace settings
+/c local surface = game.player.surface
+local patches = {"iron-ore", "copper-ore", "coal", "stone", "uranium-ore"}
+local autoplace = surface.map_gen_settings.autoplace_controls
+for _, resource in pairs(patches) do
+  autoplace[resource].size = autoplace[resource].size + 1
+  autoplace[resource].frequency = autoplace[resource].frequency * 2
+  autoplace[resource].richness = 0.5
+end
+surface.map_gen_settings.autoplace_controls = autoplace
