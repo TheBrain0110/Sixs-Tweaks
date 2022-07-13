5955129818947af3d18c3cf16513991e8a4e5e4e
TheBrain0110 <tonofpudding@gmail.com>
2022-07-12 21:19:06 -0400

    Methane ice uses less Cryonite slush

--- prototypes/recipe/spacex_early.lua
+++ prototypes/recipe/spacex_early.lua
@@ -1,1 +1,4 @@
+local data_util = require("data_util")
+
 se_core_fragment_resources["raw-rare-metals"] = { multiplier = 1.25, omni_multiplier = 0.25}
+data_util.replace_or_add_ingredient("se-cryonite-to-methane-ice", "se-cryonite-slush", "se-cryonite-slush", 1, true)
